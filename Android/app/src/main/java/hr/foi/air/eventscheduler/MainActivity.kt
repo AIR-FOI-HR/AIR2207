925ac9d168313f9a36fe33234b2fdb8a69f76d48
dmujanovic <dmujanovi@foi.hr>
2022-12-14 23:02:02 +0100

    Created bottom navigation bar with 5 buttons, each one when pressed opens up different fragment. Fragments display different text and color just for basic display of working functionality.

--- Android/app/src/main/java/hr/foi/air/eventscheduler/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/eventscheduler/MainActivity.kt
@@ -1,11 +1,43 @@
 package hr.foi.air.eventscheduler
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import hr.foi.air.eventscheduler.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(News())
+
+
+        binding.nav.setOnItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.News -> replaceFragment(News())
+                R.id.Artists -> replaceFragment(Artists())
+                R.id.info -> replaceFragment(Info())
+                R.id.Settings -> replaceFragment(Settings())
+                R.id.Timetable -> replaceFragment(Timetable())
+                else -> {
+
+                }
+            }
+            true
+
+        }
+
+    }
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
